39699b98d5c5eb73481c31ae487e604924158faf
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-11-14 15:32:44 +0100

    Add project files.

---
+++ Uppgift_3_2__/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_3_2__
+{
+    class Program
+    {
+
+        static void Main(string[] args) 
+            {
+                Console.WriteLine("har du gått ut gymnasiet? (j/n):");
+                string gymnasiesvar = Console.ReadLine();
+
+                Console.WriteLine("Hur gammal är du");
+                int ålder=int.Parse(Console.ReadLine());
+
+                if (gymnasiesvar == "j" && ålder < 22)
+                {
+                    Console.WriteLine("Vi vill gärna anställa dig");
+
+                }
+                else
+                {
+                    Console.WriteLine("tyvärr vi letar efter en annan personl ");
+                }
+               
+             
+            }
+    }
+}
